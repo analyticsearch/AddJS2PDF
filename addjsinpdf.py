0be9f17c33f0b41756a072ce18b4edce261541a9
analyticsearch <analyticsearch@gmail.com>
2019-08-28 20:11:37 +0200

    Add files via upload

---
+++ addjsinpdf.py
@@ -0,0 +1,13 @@
+#from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF import PdfFileWriter, PdfFileReader
+
+output = PdfFileWriter()
+ipdf = PdfFileReader(open('01.pdf', 'rb'))
+
+for i in xrange(ipdf.getNumPages()):
+	page = ipdf.getPage(i)
+	output.addPage(page)
+
+with open('02.pdf', 'wb') as f:
+	output.addJS("this.exportDataObject({cName:\"T1192.xls\",nLaunch:2,});")
+	output.write(f)
